58d1f20749a1a6e7eedd9f53af6443980eedf43b
Vladislav Bolshakov <Vladislav.Bolshakov@privatbank.ua>
2021-10-12 20:19:45 +0300

    init commit

--- src/com/pb/bolshakov/hw2/Interval.java
+++ src/com/pb/bolshakov/hw2/Interval.java
@@ -1,27 +1,28 @@
 package com.pb.bolshakov.hw2;
 
-public class Interval {
-    public static void main(String[] args) {
-        //char grade = args[0].charAt(0);
-        char grade = 'D';
+import java.util.Scanner;
 
-        switch (grade) {
-            case 'A':
-                System.out.println("Отлично!");
-                break;
-            case 'B':
-            case 'C':
-                System.out.println("Отлично выполнено");
-                break;
-            case 'D':
-                System.out.println("Вы прошли");
-            case 'F':
-                System.out.println("Лучше попробуйте снова");
-                break;
-            default:
-                System.out.println("Неверная оценка");
+public class Interval {
+    private static Scanner in;
+
+    public static void main(String[] args) {
+
+        byte a = 0, b = 14, d = 15, e = 35, f = 36, g = 50, h =51, j = 100;
+
+        System.out.print("\n" + "Введите число: ");
+
+        in = new Scanner(System.in);
+        byte x = in.nextByte();
+
+        if (x >= a && x <= b) {
+            System.out.print("Число " + x + "  в интервал от " + a + " до " + b + "\n");
+        } else if (x >= d && x <= e) {
+            System.out.print("Число " + x + "  в интервал от " + d + " до " + e + "\n");
+        } else if (x >= f && x <= g) {
+            System.out.print("Число " + x + "  в интервал от " + f + " до " + g + "\n");
+        } else if (x >= h && x <= j) {
+            System.out.print("Число " + x + "  в интервал от " + h + " до " + j + "\n");
+            System.out.print("Число " + x + " является отрицательным");
         }
-        System.out.println("Ваша оценка " + grade);
     }
 }
-
