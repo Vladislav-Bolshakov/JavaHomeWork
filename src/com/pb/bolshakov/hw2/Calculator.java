0d7c8e740028ebc1e1be5c707e6f84a81aa973ba
Vladislav Bolshakov <Vladislav.Bolshakov@privatbank.ua>
2021-10-12 19:52:54 +0300

    init commit

---
+++ src/com/pb/bolshakov/hw2/Calculator.java
@@ -0,0 +1,63 @@
+package com.pb.bolshakov.hw2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int operand1 = getInt();
+        int operand2 = getInt();
+        char operation = getOperation();
+        int result = calc(operand1,operand2,operation);
+        System.out.println("Результат операции: "+result);
+    }
+
+    public static int getInt(){
+        System.out.println("Введите число:");
+        int operation;
+        if(scanner.hasNextInt()){
+            operation = scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next();
+            operation = getInt();
+        }
+        return operation;
+    }
+
+    public static char getOperation(){
+        System.out.println("Введите операцию:");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе данных. Попробуйте еще раз.");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static int calc(int operand1, int operand2, char operation){
+        int result;
+        switch (operation){
+            case '+':
+                result = operand1+operand2;
+                break;
+            case '-':
+                result = operand1-operand2;
+                break;
+            case '*':
+                result = operand1*operand2;
+                break;
+            case '/':
+                result = operand1/operand2;
+                break;
+            default:
+                System.out.println("Операция не найдена. Повторите ввод еще раз.");
+                result = calc(operand1, operand2, getOperation());
+        }
+        return result;
+    }
+}
---
+++ src/com/pb/bolshakov/hw2/Interval.java
@@ -0,0 +1,27 @@
+package com.pb.bolshakov.hw2;
+
+public class Interval {
+    public static void main(String[] args) {
+        //char grade = args[0].charAt(0);
+        char grade = 'D';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Отлично!");
+                break;
+            case 'B':
+            case 'C':
+                System.out.println("Отлично выполнено");
+                break;
+            case 'D':
+                System.out.println("Вы прошли");
+            case 'F':
+                System.out.println("Лучше попробуйте снова");
+                break;
+            default:
+                System.out.println("Неверная оценка");
+        }
+        System.out.println("Ваша оценка " + grade);
+    }
+}
+
