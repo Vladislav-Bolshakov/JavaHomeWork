efd2a4a849639ac7d7e1434298d874e455aa2aff
Vladislav Bolshakov <Vladislav.Bolshakov@privatbank.ua>
2021-10-15 19:34:54 +0300

    init commit

--- src/com/pb/bolshakov/hw2/Calculator.java
+++ src/com/pb/bolshakov/hw2/Calculator.java
@@ -3,61 +3,50 @@ package com.pb.bolshakov.hw2;
 import java.util.Scanner;
 
 public class Calculator {
-    static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int operand1 = getInt();
-        int operand2 = getInt();
-        char operation = getOperation();
-        int result = calc(operand1,operand2,operation);
-        System.out.println("Результат операции: "+result);
-    }
+        Scanner scan = new Scanner(System.in);
 
-    public static int getInt(){
-        System.out.println("Введите число:");
-        int operation;
-        if(scanner.hasNextInt()){
-            operation = scanner.nextInt();
-        } else {
-            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
-            scanner.next();
-            operation = getInt();
-        }
-        return operation;
-    }
+        int operand1;
+        int operand2;
 
-    public static char getOperation(){
-        System.out.println("Введите операцию:");
-        char operation;
-        if(scanner.hasNext()){
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Вы допустили ошибку при вводе данных. Попробуйте еще раз.");
-            scanner.next();
-            operation = getOperation();
-        }
-        return operation;
-    }
+        System.out.print("Введите первое число");
+        operand1 = scan.nextInt();
 
-    public static int calc(int operand1, int operand2, char operation){
-        int result;
-        switch (operation){
-            case '+':
-                result = operand1+operand2;
+        System.out.print("Введите второе число");
+        operand2 = scan.nextInt();
+
+        System.out.print("Что делаем(+-*/)?");
+        //}
+        String sign = scan.next(); // + - * /
+
+        switch (sign) {
+            case "+":
+                System.out.println(+(operand1 + operand2));
                 break;
-            case '-':
-                result = operand1-operand2;
+
+            case "-":
+                System.out.println(+(operand1 - operand2));
                 break;
-            case '*':
-                result = operand1*operand2;
+
+            case "*":
+                System.out.println(+(operand1 * operand2));
                 break;
-            case '/':
-                result = operand1/operand2;
-                break;
-            default:
-                System.out.println("Операция не верная. Повторите ввод еще раз.");
-                result = calc(operand1, operand2, getOperation());
+
+            case "/":
+                System.out.println(+(operand1 / operand2));
+            {
+                if (operand2 == 0) ;
+                System.out.println("Ошибочка. Делить на ноль нелзя");
+            }
+            break;
+
+            default: {
+                System.out.println("Неверная операция");
+            }
+
+
         }
-        return result;
+
+
     }
 }
