2ed8eb3b66b2b2654ef0555bcefcb3badfaa67a9
Vladislav-Bolshakov <92218821+Vladislav-Bolshakov@users.noreply.github.com>
2021-11-23 19:16:47 +0200

    Delete src/com/pb/bolshakov directory

--- src/com/pb/bolshakov/hw1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.pb.bolshakov.hw1;
-
-public class HelloWorld {
-        public static void main(String[] args) {
-                System.out.println("Hello World");
-        }
-}
--- src/com/pb/bolshakov/hw7/ManClothes.java
+++
@@ -1,8 +0,0 @@
-package com.pb.bolshakov.hw7;
-
-public interface ManClothes {
-    default void dressMan() {
-        System.out.println("Одеваем мужчину");
-    }
-}
-
